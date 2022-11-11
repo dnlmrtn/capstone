f09ac574f6658f247bd84b2d77c430d3d98c1127
Benjamin-Armstrong <76851080+Benjamin-Armstrong@users.noreply.github.com>
2022-11-10 22:21:50 -0500

    uncertainty 2

--- ql11-10.py
+++ ql11-10.py
@@ -8,6 +8,29 @@ def dynamics(xt = list, ut = list):
     #xnext.append(xnext2)
     return xnext
 
+#Uncertainty Function
+
+from numpy.random import seed
+from numpy.random import randint
+
+def uncertainty(xt):
+    seed(1)
+    randnum = randint(0,100,14)
+    for i in range (0, len(xt)):
+        #For Positive effects and decrease
+        if i <= 7 and 3 <= randnum[i] <= 10:
+            xt[i] -= 1
+        #For Positive effects and increase    
+        if i <= 7 and 0 <= randnum[i] <= 2:
+            xt[i] += 1
+        #For Negative effects and decrease
+        if i > 7 and 3 <= randnum[i] <= 10:
+            xt[i] -= 1
+        #For Negative effects and increase    
+        if i > 7 and 0 <= randnum[i] <= 2:
+            xt[i] += 1
+    return xt
+
 def costFN(xCurrent,uCurrent,uPast,W):
     return np.dot(xCurrent,W) + 0.155*uCurrent
 
--- treatment_model.py
+++ treatment_model.py
@@ -8,3 +8,25 @@ def dynamics(xt, ut):
     xnext.append(xnext2)
     return xnext
 
+#Uncertainty Function
+import numpy
+from numpy.random import seed
+from numpy.random import randint
+
+def uncertainty(xt):
+    seed(1)
+    randnum = randint(0,100,14)
+    for i in range (0, len(xt)):
+        #For Positive effects and decrease
+        if i <= 7 and 3 <= randnum[i] <= 10:
+            xt[i] -= 1
+        #For Positive effects and increase    
+        if i <= 7 and 0 <= randnum[i] <= 2:
+            xt[i] += 1
+        #For Negative effects and decrease
+        if i > 7 and 3 <= randnum[i] <= 10:
+            xt[i] -= 1
+        #For Negative effects and increase    
+        if i > 7 and 0 <= randnum[i] <= 2:
+            xt[i] += 1
+    return xt
