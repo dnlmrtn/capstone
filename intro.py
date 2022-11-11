ccf077af5748157e9c88bd471a68acc3d823ed3f
Benjamin-Armstrong <76851080+Benjamin-Armstrong@users.noreply.github.com>
2022-11-10 22:19:30 -0500

    uncertainty

--- intro.py
+++ intro.py
@@ -1,8 +1,6 @@
 print("testing")
 print("hello world")
-<<<<<<< HEAD
 print("Ryan")
-=======
 print("Ryan")
 print("Test 2")
 print("test 3")
@@ -11,4 +9,3 @@ print("test 3")
 ## Environment Structure
 
 ## Graph Display
->>>>>>> 171f97b6273d9a9f8cbc194017c14d5359fb8d29
